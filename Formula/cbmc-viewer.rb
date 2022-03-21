66b007728dfa92870ea823779ae7c6c86d9fd23c
aws-viewer-for-cbmc-release-ci <100316598+aws-viewer-for-cbmc-release-ci@users.noreply.github.com>
2022-03-21 18:15:03 +0000

    cbmc-viewer 2.18

--- Formula/cbmc-viewer.rb
+++ Formula/cbmc-viewer.rb
@@ -3,12 +3,12 @@ class CbmcViewer < Formula
   desc "Scans the output of CBMC and produces a browsable summary of the results"
   homepage "https://github.com/awslabs/aws-viewer-for-cbmc"
   url "https://github.com/ronakfof/viewer.git",
-      tag:      "viewer-2.17",
-      revision: "10caed5bfa9db1b2ef641dfea9cc2336813dc51b"
+      tag:      "viewer-2.18",
+      revision: "9893b9d6e21c23da49ad46aa199d40505877806a"
   license "Apache-2.0"
 
   bottle do
-    root_url "https://github.com/ronakfof/viewer/releases/download/viewer-2.17"
+    root_url "https://github.com/ronakfof/viewer/releases/download/viewer-2.18"
     sha256 cellar: :any_skip_relocation, big_sur:      "945be88c7f1e7dc224501001e2fafe06621ee61f9991c7e365d9395b1591b0d0"
     sha256 cellar: :any_skip_relocation, catalina:     "6dbab41477335a880d9b91f8138209619c8af2af662eea977094fe7629daedcd"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "9253659b68fa60cb16161acea59c31518d920d07f1c71a1c739643f07d26d061"
